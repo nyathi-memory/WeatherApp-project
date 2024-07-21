620a7ea1db035d3ea5ba05caf2694c342a54be62
nyathi-memory <miminy56@gmail.com>
2024-07-21 22:14:44 +0200

    Added basic java

--- source/script.js
+++ source/script.js
@@ -1,1 +1,8 @@
-
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchIput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchIput.value;
+  }
+  let searchFormElement =document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
